59ec1e849820bbcaa6622e5e4c7ca3f7c44d06e3
eraybulut34 <abdullaheraybulut@hotmail.com>
2025-02-26 13:00:20 +0300

    fix: vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,35 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import dts from 'vite-plugin-dts'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    dts({
-      insertTypesEntry: true,
-      include: ['src/components/**/*.vue', 'src/types/**/*.ts']
-    })
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/components/VueGridify.vue'),
-      name: 'VueGridify',
-      fileName: (format) => `vue-gridify.${format}.js`
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue'
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+  const isLibrary = mode === 'library'
+
+  return {
+    plugins: [
+      vue(),
+      dts({
+        insertTypesEntry: true,
+        include: ['src/components/**/*.vue', 'src/types/**/*.ts']
+      })
+    ],
+    build: isLibrary ? {
+      lib: {
+        entry: resolve(__dirname, 'src/components/VueGridify.vue'),
+        name: 'VueGridify',
+        fileName: (format) => `vue-gridify.${format}.js`
+      },
+      rollupOptions: {
+        external: ['vue'],
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
         }
       }
+    } : {
+      outDir: 'dist',
+      assetsDir: 'assets'
     }
   }
-}) 
+})
